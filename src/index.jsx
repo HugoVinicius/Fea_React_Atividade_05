d576464ab95507228614f6d952b1bb2cecd752bb
HugoVinicius <hugo.desenvolvimento@outlook.com>
2019-07-04 22:29:58 -0300

    Terceira parte exercicio

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import ReactDOM from 'react-dom'
 
+import './img/favicon.ico'
+
 ReactDOM.render(
     <h1>Desenvolvimento Front-end Avançado</h1>,
     document.querySelector("#main")
--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(jpe?g|ico|png|gif|svg)$/i,
+                loader: 'file-loader?name=img/[name].[ext]'
             }
         ]
     }
